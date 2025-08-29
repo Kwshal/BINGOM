60a61acc8f82e1e9e13df2811fffbea12c577745
kwshal <kushal3738@gmail.com>
2025-08-29 14:34:07 +0530

    show winner, reset for all client, remove filler dots, reset chat, remove pointer events on win, show player's turn.

--- db.js
+++ db.js
@@ -1,11 +1,8 @@
-import { syncNums, msgUpdaterFunction, toogleChat, showWinner } from "./script.js";
-// Import the functions you need from the SDKs you need
+import { syncNums, updateMessages, toogleChat, showWinner, reset, showTurn } from "./script.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import { getDatabase, ref, onValue, set } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyABxgXrl0ZawYX4U_Bqpt9HRVgSaW1CA7k",
     authDomain: "bingo-ace.firebaseapp.com",
@@ -19,45 +16,61 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-const dbRef = ref(db, 'bingom/num');
-const dbRef2 = ref(db, 'bingom/msgs');
-const dbRef3 = ref(db, 'bingom/chatState');
-const dbRef4 = ref(db, 'bingom/winner');
+
+const numRef = ref(db, 'bingom/num');
+const msgsRef = ref(db, 'bingom/msgs');
+const chatStateRef = ref(db, 'bingom/chatState');
+const winnerRef = ref(db, 'bingom/winner');
+const resetRef = ref(db, 'bingom/reset');
+const playerRef = ref(db, 'bingom/player');
 
 
-let updateNums = (num) => {
+let setNums = (num) => {
     // console.log(num);
-    set(dbRef, num);
+    set(numRef, num);
 
 }
-let msgFunction = (msg) => {
-    set(dbRef2, msg);
+let setMessage = (msg) => {
+    set(msgsRef, msg);
 }
-let chatStateUpdater = (val) => {
-    set(dbRef3, val);
+let setChatState = (state) => {
+    set(chatStateRef, state);
 }
-let setWinner = (val) => {
-    set(dbRef4, val);
+let setWinner = (player) => {
+    set(winnerRef, player);
+}
+let resetGame = (val) => {
+    set(resetRef, val);
+}
+let setPlayer = (player) => {
+    set(playerRef, player);
 }
 
-onValue(dbRef, (snapshot) => {
+onValue(numRef, (snapshot) => {
     const data = snapshot.val();
-    // console.log(data);
     syncNums(data);
 });
-onValue(dbRef2, (snapshot) => {
+onValue(msgsRef, (snapshot) => {
     const data = snapshot.val();
-    data && msgUpdaterFunction(data);
+    data && updateMessages(data);
 });
-onValue(dbRef3, (snapshot) => {
+onValue(chatStateRef, (snapshot) => {
     const data = snapshot.val();
     toogleChat(data);
     data == "open" && document.getElementById("chat-inp").focus();
     // console.log(data);
 });
-onValue(dbRef4, (snapshot) => {
+onValue(winnerRef, (snapshot) => {
     const data = snapshot.val();
     showWinner(data);
 });
+onValue(resetRef, (snapshot) => {
+    const data = snapshot.val();
+    data && reset();
+});
+onValue(playerRef, (snapshot) => {
+    const data = snapshot.val();
+    showTurn(data);
+});
 
-export { updateNums, msgFunction, chatStateUpdater, setWinner };
+export { setNums, setMessage, setChatState, setWinner, resetGame, setPlayer };
--- script.js
+++ script.js
@@ -1,72 +1,69 @@
-import { updateNums, msgFunction, chatStateUpdater, setWinner } from "./db.js";
+import { setNums, setMessage, setChatState, setWinner, resetGame, setPlayer } from "./db.js";
 
+const gameBoard = document.getElementById("game-board");
 const cells = document.querySelectorAll(".cell");
 const winStatus = document.getElementById("win-status");
-
-const instruction = document.getElementById("instruction");
 const resetBtn = document.getElementById("reset-btn");
-const gameBoard = document.getElementById("game-board");
-
-let gameRunning = false;
-// let won = false;
+const turn = document.getElementById("turn");
+const header = document.getElementById("header");
 
 let randomChars = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 ]
 
-// let specialNumsList = [1, 25, 11, 9];
+let username = localStorage.getItem("username") || "Nameless";
+document.addEventListener("DOMContentLoaded", () => {
+    if (username != "Nameless") {
+        let name = document.getElementById("name");
+        name.textContent = username;
+        name.style.backgroundColor = "transparent";
+        name.style.color = "white";
+        header.style.backgroundColor = "transparent";
+        header.style.color = "white";
+        // document.getElementById("edit-svg").style.display = "none";
+    }
+});
 
-
-resetBtn.addEventListener("click", reset);
+resetBtn.addEventListener("click", resetGame);
 
 function init() {
-    gameRunning = true;
     shuffle();
     cells.forEach((cell) => {
         cell.addEventListener("click", handleClick);
         cell.style.transition = `color ${cell.getAttribute("index") * 0.1}s`;
         cell.style.color = "transparent";
-        // cell.textContent == 5 && (cell.style.color = "#4b4b4bff");
     });
-    // resetBtn.innerHTML = "Reset";
-    // winStatus.innerHTML = "reset";
-    // instruction.style.color = "#4e6d81";
-    updateNums("");
-    msgFunction("");
-    chatStateUpdater("close");
+    setNums("");
+    setMessage("");
+    document.getElementById("chat-messages").innerHTML = "";
+    document.getElementById("name").innerHTML = username;
+    setChatState("close");
     setWinner("");
+    resetGame("");
+    setPlayer("");
     winStatus.style.top = "-100%";
-    // won = false;
+    turn.style.display = "none";
+    header.style.display = "flex";
+
 }
 init();
-
-// randomizing numbers and letters
 
 function shuffle() {
     randomize(cells, randomChars);
 };
 
-// const 
 function handleClick() {
     let cross = `<svg width="64px" height="64px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke=""><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z" fill="#4e6d81"></path> </g></svg>`;
-
-    gameRunning = true;
     if (!this.classList.contains("clicked")) {
-
-
         this.classList.add("clicked");
-        // let index = this.getAttribute("index");
-    console.log("clicked", this.textContent);
-        updateNums(this.textContent);
+        setNums(this.textContent);
         this.innerHTML = cross;
-        // this.style.color = "#4e6d81";
-        // iA[index] = "w";
         checkBingo();
+        setPlayer(username);
+        turn.style.display = "block";
+        header.style.display = "none";
+        // console.log(username);
     }
-    // this.style.pointerEvents = "none";
-    // instruction.style.color = "transparent";
-    // console.log(iA);
-
 }
 
 function syncNums(num) {
@@ -109,37 +106,13 @@ function checkBingo() {
                 break;
             }
         }
-        if (won) {
-            count++;
-        }
+        won && count++;
     }
-    let circle = document.createElement("span");
-    circle.id = "circle";
 
     if (count >= 5) {
-        // won = true;
-        // gameRunning = false;
-
-        cells.forEach((cell) => {
-            // if (specialNumsList.includes(Number(cell.textContent))) {
-            //     cell.style.color = "#4e6d81699";
-
-            // }
-            (!cell.classList.contains("clicked")) && (cell.appendChild(circle.cloneNode()));
-            cell.style.pointerEvents = "none";
-
-        });
-        // instruction.style.color = "#4e6d81";
-        resetBtn.style.display = "inline";
-        winStatus.innerHTML = "You Win!";
-
-        winStatus.style.top = "5%";
-        // console.log("Congratulations! You won!");
-        chatStateUpdater("open");
-        msgFunction(`<span id="username">${username}:</span> BINGO! I win!!`);
+        setChatState("open");
+        setMessage(`<span id="username">${username}:</span> BINGO! I win!!`);
         setWinner(username);
-        // let shadow = document.querySelector("#game-board::after");
-        // shadow.style.display = "block";
     }
 
 
@@ -158,13 +131,7 @@ function reset() {
         cell.classList.remove("clicked");
     });
     init();
-    msgFunction("Game Reset. New Round!");
-    // document.querySelector("#game-board::after").style.display = "none";
     winStatus.style.display = "none";
-    // resetBtn.innerText = "Start";
-    // currentFunc = "start";
-    // winStatus.innerHTML = "start";
-    // instruction.style.color = "transparent";
 }
 
 function randomize(list, randomNum) {
@@ -193,8 +160,8 @@ chatBtn.addEventListener("click",
     }
 );
 
-chatInp.addEventListener("blur", () => chatStateUpdater("close"));
-chatInp.addEventListener("focus", () => chatStateUpdater("open"));
+chatInp.addEventListener("blur", () => setChatState("close"));
+chatInp.addEventListener("focus", () => setChatState("open"));
 chatInp.addEventListener("keypress", sendText);
 
 function toogleChat(chatState) {
@@ -206,20 +173,9 @@ function toogleChat(chatState) {
     // gameBoard.style.filter = gameBoard.style.filter == "blur(2px) opacity(0.5)" ? "blur(0px) opacity(1)" : "blur(2px) opacity(0.5)";
 }
 
-let username = localStorage.getItem("username") || "Nameless";
-
-document.addEventListener("DOMContentLoaded", () => {
-    if (username != "Nameless") {
-        let name = document.getElementById("name");
-        name.textContent = username;
-        name.style.backgroundColor = "transparent";
-        name.style.color = "white";
-
-    }
-});
-
 let editSvg = document.getElementById("edit-svg");
 editSvg.addEventListener("click", () => {
+    let header = document.getElementById("header");
     let name = document.getElementById("name");
     name.contentEditable = true;
     name.focus();
@@ -228,43 +184,56 @@ editSvg.addEventListener("click", () => {
         username = name.textContent.trim() || "Nameless";
         localStorage.setItem("username", username);
         name.textContent = username;
+        editSvg.style.display = "none";
+        header.style.backgroundColor = "transparent";
+        name.style.backgroundColor = "transparent";
+        name.style.color = "white";
     });
 });
 
 function sendText(e) {
     if (e.key == "Enter") {
         let text = chatInp.value;
-        text.trim() && msgFunction(`<span id="username">${username}</span>: ` + text);
+        text.trim() && setMessage(`<span id="username">${username}</span>: ` + text);
         chatInp.value = "";
     }
 }
 
-function msgUpdaterFunction(msg) {
+function updateMessages(msg) {
     let li = document.createElement("li");
     li.id = "message";
     li.innerHTML = msg;
     document.getElementById("chat-messages").appendChild(li);
-    msgFunction("");
+    setMessage("");
 }
 
 function showWinner(winner) {
     let username = localStorage.getItem("username") || "Nameless";
+    winStatus.style.top = "5%";
     if (winner == username) {
         winStatus.innerHTML = "You Win!";
     } else if (winner && winner != username) {
         winStatus.innerHTML = `${winner} Wins!`;
     }
+    winner && document.querySelectorAll(".cell").forEach((cell) => {
+        cell.style.pointerEvents = "none";
+    });
+}
+function showTurn(player) {
+    if (player == "") {
+        cells.forEach((cell) => cell.style.pointerEvents = "all");
+    } else if (player == username) {
+        turn.innerHTML = `Opponent's Turn!`;
+        cells.forEach((cell) => cell.style.pointerEvents = "none");
+    } else {
+        turn.innerHTML = `Your Turn!`;
+        cells.forEach((cell) => cell.style.pointerEvents = "all");
+    }
 }
 
+export { syncNums, updateMessages, toogleChat, showWinner, reset, showTurn };
 
-
-// HTML
-
-
-export { syncNums, msgUpdaterFunction, toogleChat, showWinner };
-
-
-
+// PWA install prompt
 let deferredPrompt;
 document.addEventListener("DOMContentLoaded", () => {
     let installBtn = document.getElementById("install-b");
@@ -288,3 +257,4 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+localStorage.removeItem("username");
